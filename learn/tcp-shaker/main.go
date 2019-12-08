47e5115fe44e3fb508bd8f9e4d5a1275ac68d74f
annprog <tecbbs@qq.com>
2019-12-08 18:45:19 +0800

    wip: learn tcp-shaker. use Mutex

--- learn/tcp-shaker/main.go
+++ learn/tcp-shaker/main.go
@@ -9,6 +9,7 @@ import (
 
 var ips = []string{"127.0.0.1", "192.168.0.1", "10.2.3.4", "10.5.6.7"}
 var port = "80"
+var mu sync.Mutex
 
 func Scanner() {
 	var addresses = make([]string, 0)
@@ -25,16 +26,18 @@ func Scanner() {
 	// 等待执行完成
 	wg.Wait()
 	log.Printf("notReadyAddress: %v\n", notReadyAddress)
-	log.Printf("Addresses: %v\n", addresses)
+	//log.Printf("Addresses: %v\n", addresses)
 }
 
 func ScanPort(ip string, addresses *[]string, notReadyAddress *[]string, wg *sync.WaitGroup) {
-	log.Println("scaning ", ip, "port", port)
+	//log.Println("scaning ", ip, "port", port)
 	_, err := net.DialTimeout("tcp", ip+":"+port, time.Millisecond*100)
 
 	if err != nil {
+		//log.Println("notReadyAddress: ", ip, "errMsg: ", err)
+		mu.Lock()
 		*notReadyAddress = append(*notReadyAddress, ip)
-		log.Println("notReadyAddress: ", ip, "errMsg: ", err)
+		mu.Unlock()
 	} else {
 		*addresses = append(*addresses, ip)
 	}
@@ -45,6 +48,6 @@ func ScanPort(ip string, addresses *[]string, notReadyAddress *[]string, wg *syn
 func main() {
 	for {
 		Scanner()
-		time.Sleep(3 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
